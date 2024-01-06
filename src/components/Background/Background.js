440f230e4d81d3f284d95e8f3f2a39d22cae7e6f
NervzZ <julien.jordan@epfl.ch>
2024-01-06 08:44:36 +0100

    interactive background

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Background.css';
 
 const Background = () => {
+    const [gradientPosition, setGradientPosition] = useState({ x: 50, y: 50 });
+
+    const handleMouseMove = (e) => {
+        const x = (e.clientX / window.innerWidth) * 100;
+        const y = (e.clientY / window.innerHeight) * 100;
+        setGradientPosition({ x, y });
+    };
+
     return (
-        <div className="background"></div>
+        <div
+            className="background"
+            onMouseMove={handleMouseMove}
+            style={{
+                backgroundPosition: `${gradientPosition.x}% ${gradientPosition.y}%`
+            }}
+        ></div>
     );
 };
 
