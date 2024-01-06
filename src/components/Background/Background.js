e3ff2847fc9f593f25994bd6a854f6052df18928
NervzZ <julien.jordan@epfl.ch>
2024-01-06 08:04:56 +0100

    chatGPT attempt at animated background

---
+++ src/components/Background/Background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Background.css';
+
+const Background = () => {
+    return (
+        <div className="background">
+            <div className="circle"></div>
+            <div className="square"></div>
+            <div className="triangle"></div>
+        </div>
+    );
+};
+
+export default Background;
