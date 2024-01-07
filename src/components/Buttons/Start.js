0747c5d6d4ca2bdc29b2895a1872b1107a3203a2
NervzZ <julien.jordan@epfl.ch>
2024-01-07 10:19:18 +0100

    finished styling the start button

--- src/components/Buttons/Start.js
+++ src/components/Buttons/Start.js
@@ -3,15 +3,24 @@ import './Start.css';
 const Start = (props) => {
 
     const startStyle = {
+        height: (props.viewSize.width / 100) * 3.5,
+        width: '12%',
+        lineHeight: '165%',
+        fontSize: Math.max(props.viewSize.width / 50, 14),
         top: (props.viewSize.height / 2) - 20
+    }
+
+    const handleClick = () => {
+        console.log("clicked")
     }
 
     return (
         <div
             className="startButton"
             style={startStyle}
+            onClick={handleClick}
         >
-            Explore
+            <span>Explore</span>
         </div>
     )
 }
