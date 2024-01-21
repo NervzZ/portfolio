f75e84901be68c1624acf75aa1b01e0d5243c06b
NervzZ <julien.jordan@epfl.ch>
2024-01-21 19:45:05 +0100

    changed back button to home

--- src/components/Buttons/HomeButton/HomeButton.js
+++ src/components/Buttons/HomeButton/HomeButton.js
@@ -12,10 +12,10 @@ const HomeButton = (props) => {
             className="BackArrow"
             onClick={handleClick}
             xmlns="http://www.w3.org/2000/svg"
-            width="3%"
+            width="2%"
             viewBox="0 -960 960 960"
         >
-            <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/>
+            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
         </svg>
     )
 }
---
+++ src/components/TextBox/TextBox.js
@@ -0,0 +1,13 @@
+import './TextBox.css'
+import React from "react";
+
+const TextBox = (props) => {
+
+    return (
+        <div className="TextBox">
+
+        </div>
+    )
+}
+
+export default TextBox
